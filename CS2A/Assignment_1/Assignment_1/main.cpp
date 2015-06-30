219653173ac4e81bdb13998abf06293d773d5722
daariff <aariff.deen@gmail.com>
2015-06-30 10:24:45 -0700

    New Folder

---
+++ CS2A/Assignment_1/Assignment_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment_1
+//
+//  Created by Deen Aariff on 6/30/15.
+//  Copyright (c) 2015 Deen Aariff. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
