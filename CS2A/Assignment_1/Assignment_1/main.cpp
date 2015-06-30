11f990d9769d475ac415d6071ebd34f981e281de
daariff <aariff.deen@gmail.com>
2015-06-30 10:32:12 -0700

    Hello World

--- CS2A/Assignment_1/Assignment_1/main.cpp
+++ CS2A/Assignment_1/Assignment_1/main.cpp
@@ -7,9 +7,9 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-   // insert code here...
-   std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+   cout << "Hello World\n";
+   return 0;
 }
