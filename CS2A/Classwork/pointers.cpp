b52d02b0a197eba6d2090a87751bb7cb95986c5e
daariff <aariff.deen@gmail.com>
2015-07-02 11:43:04 -0700

    edits

--- CS2A/Classwork/pointers.cpp
+++ CS2A/Classwork/pointers.cpp
@@ -6,8 +6,19 @@ using namespace std;
 
 int main() {
 	int j = 10;
-	long i = (long) &j;
+	// long i = (long) &j;
+	char *p = (char *) &j;
 
 	cout << "The pointer to the variable to j is " << &j << "\n";
-	cout << "i = " << i <<endl;
+	cout << "j = " << j <<endl;
+	cout <<"*p = " << (int) (*p) << endl;
+
+	// What does that tell us about the ENDIAN-NESS of the MAC?
+
+	// Now change the value of j indirectly
+	*(p+1) = 1;
+	
+	// What is j now? Predict and then run to check.
+	cout << "j is now " << j << endl;
+	return 0;	
 }
