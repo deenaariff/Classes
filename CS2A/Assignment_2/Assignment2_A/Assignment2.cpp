3af102d02f6c8f6ff7b46b99b2d4208975dcb125
daariff <aariff.deen@gmail.com>
2015-07-02 19:11:50 -0700

    Completed Initial Part of Sterling's Approximation

--- CS2A/Assignment_2/Assignment2_A/Assignment2.cpp
+++ CS2A/Assignment_2/Assignment2_A/Assignment2.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 
 int main () {
-	  //Initialize studID and numLet
+	//Initialize studID and numLet
   	int studID = 20208433;
   	int numLet = 6;
   	// evaluate answer to questions
   	int eval1 = studID % 2; // value of question 1
   	int eval2 = studID % numLet; // value of question 2
-  	int eval3 = studID / numLet; // value of question 3
+  	double eval3 = (double) studID / numLet; // value of question 3
   	int eval4 = (numLet * (1 + numLet)) / 2; // eval4 calculated using sum of Arithmetic Series (n(a1+an))/2
   	int eval5InParenth = (studID - 54321) / pow ((numLet + 30),2); // parenthesis in denominator
-  	int eval5 = 100000 / (99 + eval5InParenth); // value of question 5 
+  	double eval5 = 100000.0 / (99 + eval5InParenth); // value of question 5 
   	// print out values
   	cout << "My last name is Aariff \n";
   	cout << "My Student ID is " << studID << "\n";
   	cout << "The number of characters in my last name is " << numLet << "\n";
   	cout << "Expression #1 ----------: " << eval1 << "\n";
-    cout << "Expression #2 ----------: " << eval2 << "\n";
- 	  cout << "Expression #3 ----------: " << eval3 << "\n";
-    cout << "Expression #4 ----------: " << eval4 << "\n";
-    cout << "Expression #5 ----------: " << eval5 << "\n";
+        cout << "Expression #2 ----------: " << eval2 << "\n";
+        cout << "Expression #3 ----------: " << eval3 << "\n";
+        cout << "Expression #4 ----------: " << eval4 << "\n";
+        cout << "Expression #5 ----------: " << eval5 << "\n";
 }
 
 /*-----------------------------RUN---------------------------------------
@@ -31,9 +31,9 @@ My Student ID is 20208433
 The number of characters in my last name is 6
 Expression #1 ----------: 1
 Expression #2 ----------: 1
-Expression #3 ----------: 3368072
+Expression #3 ----------: 3.36807e+06
 Expression #4 ----------: 21
-Expression #5 ----------: 6
+Expression #5 ----------: 6.38978
 
 ------------------------------------------------------------------------*/
 
