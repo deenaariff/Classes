a46373e3c650ecf1d67d57a521f1f44f8ce44ec5
daariff <aariff.deen@gmail.com>
2015-07-06 10:52:20 -0700

    Some Practice

--- CS2A/Assignment_3/Assign_3_B2.cpp
+++ CS2A/Assignment_3/Assign_3_B2.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int main() {
     // define names of ingredients and nutrients
-    string ingredients [] = {"Cowpeas"};
+    string ingredients [] = {"Cowpeas","Cinnamon"};
     string nutrients [] = {"Calories", "Fiber", "Sugars", "Saturated Fat"};
-    // array for amount of each ingredient
+    // array for amount of each ingredient and totals of each nutrition element
     int amount [5];
+    int totals [5];
     // nutrition values for nutrition types (index correspond to ingredient)
     const double NUTRITION [][sizeof(ingredients)] = {
         {200}, // Calories 
@@ -16,10 +17,8 @@ int main() {
         {5.7}, // Sugars
         {0.2}, // Saturated Fat
     };
-    // totals of each nutrition element
-    int totals [5];
     // populate amounts (n) 
-    for (int i=0; i <= sizeof(ingredients); i++) {
+    for (int i=0; i < sizeof(ingredients); i++) {
          cout << "How many grams of: " << ingredients[i] << "?\n";	 
          cin>>amount[i];
          cin.ignore();
---
+++ CS2A/Classwork/setType.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (int argc, const char * argv[]) {
+	int i;
+	cout <<"Size of int = " << sizeof(i) << endl;
+	cout <<"Size of int = " << sizeof(int) <<endl;
+	cout <<"Max int = " << INT_MAX << endl;
+	return 0;
+}
