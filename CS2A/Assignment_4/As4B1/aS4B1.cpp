eed813848f498114e1569e22453bcbc5fd41c830
daariff <aariff.deen@gmail.com>
2015-07-09 19:37:11 -0700

    Newest updates

--- CS2A/Assignment_4/As4B1/aS4B1.cpp
+++ CS2A/Assignment_4/As4B1/aS4B1.cpp
@@ -1,55 +1,57 @@
 #include <iostream>
 #include <sstream>
+#include <string>
 using namespace std;
 
 int main () {
 	// define customer variables
 	int totalCredits = 0;
 	// define program variables
-	string userInput;
-	int userInputInt, indicator;
+	string userInput, firstChar, indicator;
+	int userInputInt;
 	// define constants
 	const string ERROR_1 = "*** Use P or S, please ***";
 	const string ERROR_2 = "*** Use Y or N, please ***";
 	const string MENU = "Menu: \n \t P (process Purchase) \n \t S (Shut down)\n";
 	// Perpetual Loop (Exit with Control C)
-	while (indicator != 2) {
-		indicator = 0;
+	while (indicator != "terminate") {
+		indicator = "buy"; // initalize indicator as "buy"
 		cout<<MENU; // display menu
-		getline(cin,userInput); // take user input
-		
+		cin>>userInput;
+		firstChar = userInput.at(0); // takes first character
+	
 		// P Output
-		if (&userInput.at(0) == "p" || &userInput.at(0) == "P") {
-			return 0; //terminates program 
+		if (firstChar == "s" || firstChar == "S") {
+			indicator = "terminate"; //terminates program 
 		}
 	
 		// S Logic
-		if (totalCredits == 10) {
-				cout<<"You qualify for a free yogurt. Would you like to use your credits? (Y or N)";
-				cin<<userInput;
+		if (totalCredits >= 10) {
+				cout<<"You qualify for a free yogurt. Would you like to use your credits? (Y or N) \n";
+				cin>>userInput;
 				if (userInput == "y" || userInput == "Y") {
-					totalCredits = 0;				
+					totalCredits -= 10; // deducts credit
+					indicator = "free"; // indicates free
 				}	
-				else if (userInput == "n" || userInput == "N") {	
-				}
-				else {
-					indicator = 1;
+				else if (userInput != "n" || userInput != "N") {	
+					indicator = "error"; // disqualify users for purchase 
 				}
 		}	
-		if (&userInput.at(0) == "s" || &userInput.at(0) == "S") { 
+		if (indicator == "buy" && (firstChar == "p" || firstChar == "P")) { 
 			cout << "How many yogurts would you like to buy?: ";	
-			getline(cin,userInputInt);
+			cin >> userInputInt;
 			totalCredits += userInputInt;
 			cout << "You just earned " << userInputInt << " stamps ";
-			cout << "and have a total of" << totalCredits;
+			cout << "and have a total of " << totalCredits << "\n";
 		}
-		else if (indicator == 1) {
+		else if (indicator == "free") {
+		}
+		else if (indicator == "error") {
 			cout<<ERROR_2;
 		}
-
 		// Error Output
 		else {  // Outputs error and loops continues
-			cout<<ERROR_1;
+			cout<<ERROR_1<<"\n";
 		}
 	}
 	return 0;
